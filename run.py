26147559ce8119447e5b67575c157b06711ab705
Manan Jindal <104257504+surrealtmj@users.noreply.github.com>
2023-08-30 01:12:38 +0530

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-nameClass=input("Name of the class:")
-currentDir=os.getcwd()
-dirCommand=f"cd {currentDir}"
-execCommand=f"py -m manim main.py {nameClass} -p"
-os.system(dirCommand)
-os.system(execCommand)
